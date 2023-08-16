0490cc7aea8da5aa86c7961a659f786caefab520
Akash Gupta <akash@truefoundry.com>
2023-08-16 11:55:09 +0530

    change

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import gradio as gr
 
 def greet(name):
+    print("Hello " + name + "!")
     return "Hello " + name.capitalize() + "!"
 
 gr.Interface(fn=greet, inputs="text", outputs="text").launch(server_name='0.0.0.0', server_port=8080)
